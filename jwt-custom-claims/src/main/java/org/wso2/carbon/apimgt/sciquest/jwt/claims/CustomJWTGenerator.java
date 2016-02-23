8efd03f73893a015f8af3781e5d69fee014d4dfc
Abimaran Kugathasan <abimaran@wso2.com>
2016-02-23 10:55:43 -0500

    Added enduser as json object.

--- jwt-custom-claims/src/main/java/org/wso2/carbon/apimgt/sciquest/jwt/claims/CustomJWTGenerator.java
+++ jwt-custom-claims/src/main/java/org/wso2/carbon/apimgt/sciquest/jwt/claims/CustomJWTGenerator.java
@@ -1,17 +1,22 @@
 package org.wso2.carbon.apimgt.sciquest.jwt.claims;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.impl.dto.APIKeyValidationInfoDTO;
 import org.wso2.carbon.apimgt.impl.token.AbstractJWTGenerator;
 import org.wso2.carbon.apimgt.impl.token.ClaimsRetriever;
+import org.wso2.carbon.apimgt.impl.token.JWTGenerator;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
 
 import java.util.Calendar;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 
 public class CustomJWTGenerator extends AbstractJWTGenerator {
+    private static final Log log = LogFactory.getLog(CustomJWTGenerator.class);
     @Override
     public Map<String, String> populateStandardClaims(APIKeyValidationInfoDTO keyValidationInfoDTO, String apiContext,
                                                       String version) throws APIManagementException {
@@ -62,6 +67,43 @@ public class CustomJWTGenerator extends AbstractJWTGenerator {
     @Override
     public Map<String, String> populateCustomClaims(APIKeyValidationInfoDTO keyValidationInfoDTO, String apiContext,
                                                     String version, String accessToken) throws APIManagementException {
+        return null;
+    }
+
+    @Override
+    public String buildBody(APIKeyValidationInfoDTO keyValidationInfoDTO, String apiContext, String version,
+                            String accessToken) throws APIManagementException {
+        Map<String, String> standardClaims = populateStandardClaims(keyValidationInfoDTO, apiContext, version);
+
+        if (standardClaims != null) {
+
+            StringBuilder body = new StringBuilder();
+            body.append("{");
+
+            Iterator<Map.Entry<String, String>> entryIterator = standardClaims.entrySet().iterator();
+            while (entryIterator.hasNext()) {
+                Map.Entry<String, String> entry = entryIterator.next();
+                String key = entry.getKey();
+                if("exp".equals(key) || "nbf".equals(key) || "iat".equals(key)){
+                    //These values should be numbers.
+                    body.append("\"" + key + "\":" + entry.getValue() + ",");
+                } else if ("http://wso2.org/claims/enduser".equals(key))  { // Since enduser is a JSON object
+                    body.append("\"" + key + "\":" + entry.getValue() + ",");
+                } else{
+                    body.append("\"" + key + "\":\"" + entry.getValue() + "\",");
+                }
+
+            }
+
+            if (body.length() > 1) {
+                body.delete(body.length() - 1, body.length());
+            }
+
+            body.append("}");
+            return body.toString();
+
+        }
+
         return null;
     }
 }
